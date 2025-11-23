015c09a48dbb7f162268e954021013419e3b35e5
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-11-23 14:27:03 +0100

    Update shock_index.py

--- Algorithms/shock_index.py
+++ Algorithms/shock_index.py
@@ -3,7 +3,14 @@ import pandas as pd
 
 class ShockIndex: 
 
-    def __init__(self, vf: vitaldb.VitalFile):
+    def __init__(self, data):
+
+        if isinstance(data, vitaldb.VitalFile):
+            self._from_vf(data)
+        else:
+            self._from_df(data)
+
+    def _from_vf(self, vf):
         # Get all available track names in the VitalFile
         available_tracks = vf.get_track_names()
 
@@ -34,6 +41,38 @@ class ShockIndex:
         print("Timestamp", pre_si.head(n=5))
         #Creates the SI dataframe: Timestamp | SI_value
         self.values = {'Timestamp': pre_si["Time"], 'SI': pre_si[hr_track] / pre_si[sys_track]} 
+
+
+    def _from_df(self, list_dataframe: list[pd.DataFrame]):
+        #Se recibe una lista de dataframes
+
+        #Se sacan los indices, que son los nombres de las variables
+        available_tracks = list_dataframe.keys()
+
+        # Try to find heart rate tracks
+        hr_track = next(
+            (t for t in available_tracks if 'Intellivue/ECG_HR' in t),          # First try for ECG_HR
+            next((t for t in available_tracks if 'Intellivue/ABP_HR' in t),     # Then try for ABP_HR
+                    next((t for t in available_tracks if 'Intellivue/HR' in t), None))) # Finally try for generic HR track
+        sys_track = next(
+            (t for t in available_tracks if 'Intellivue/ABP_SYS' in t), # First try for invasive systolic BP
+            next((t for t in available_tracks if 'Intellivue/BP_SYS' in t), # Then try for another possible invasive systolic BP
+                next((t for t in available_tracks if 'Intellivue/NIBP_SYS' in t), None))) # Finally try for non-invasive systolic BP
+
+        hr = list_dataframe[hr_track] 
+        sys = list_dataframe[sys_track]
+
+        # Deletes the nan values
+        hr_clean = hr[hr["values"].notna()]     #(original: hr_track en vez de values)
+        sys_clean = sys[sys["values"].notna()]
+
+        # Creates a new dataframe with timestamp | hr_value | sys_value where both values come from the same timestamp
+        pre_si= hr_clean.merge(sys_clean, on="t_abs_ms")
+        print(pre_si)
+
+        #Creates the SI dataframe: Timestamp | SI_value
+        self.values = pd.DataFrame({'Timestamp': pre_si["t_abs_ms"], 'SI': pre_si["values_x"] / pre_si["values_y"]}) #Problema en como llamar a cada columna
+       
 
 #Handles both invasive and non-invasive blood pressure by checking available tracks.
 #Requires heart rate track, tries multiple possible names for robustness.
