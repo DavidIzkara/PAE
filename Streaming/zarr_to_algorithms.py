221692ec52c33b510c35ab3ce6c29e8f6cda191b
Eloi9 <eloi.saballs@gmail.com>
2025-11-25 00:22:42 +0100

    Update zarr_to_algorithms.py for new algorithm imports
    Refactor monitoring function to support new algorithms and improve structure.

--- Streaming/zarr_to_algorithms.py
+++ Streaming/zarr_to_algorithms.py
@@ -6,14 +6,22 @@ import numpy as np
 import pandas as pd
 
 from utils_Streaming import OUTPUT_DIR
-from utils_zarr import leer_zattrs_de_grupo, FRAME_SIGNAL_DEMO, FRAME_SIGNAL, FORMATO_TIMESTAMP, get_track_names_simplified
+from utils_zarr import leer_zattrs_de_grupo, FRAME_SIGNAL_DEMO, FRAME_SIGNAL, FORMATO_TIMESTAMP, get_track_names_simplified, escribir_prediccion, ALGORITMOS_VISIBLES
 from check_availability import check_availability
 
-from Algorithms.blood_pressure_variability import BloodPressureVariability # Este no se puede enseñar por pantalla
+#from Algorithms.blood_pressure_variability import BloodPressureVariability 
 from Algorithms.cardiac_output import CardiacOutput
-from Algorithms.heart_rate_variability import HeartRateVariability # Este no se puede enseñar por pantalla
+from Algorithms.cardiac_power_output import CardiacPowerOutput
+from Algorithms.driving_pressure import DrivingPressure
+from Algorithms.dynamic_compliance import DynamicCompliance
+from Algorithms.effective_arterial_elastance import EffectiveArterialElastance
+#from Algorithms.heart_rate_variability import HeartRateVariability 
+from Algorithms.rox_index import RoxIndex
 from Algorithms.shock_index import ShockIndex
+from Algorithms.systemic_vascular_resistance import SystemicVascularResistance
+from Algorithms.temp_comparison import TempComparison
 
+DEMO = False
 
 ZARR_PATH = os.path.join(OUTPUT_DIR, "session_data.zarr") # Ruta al archivo Zarr de ejemplo
 
@@ -159,7 +167,7 @@ def leer_ultimas_muestras_zarr(
 
 
 # Ejemplo de uso de la función recursiva
-if __name__ == "__main__":
+def main_to_loop():
     try:
         # traks_a_monitorizar = ["ECG", "NIBP_DBP", "SPO2_SAT"] # Ejemplo de tracks 
 
@@ -167,18 +175,25 @@ if __name__ == "__main__":
 
         if monitorizar_actualizacion_recursivo():
             print("\n¡ACTUALIZACIÓN DETECTADA !")
-            # time.sleep(1.0) # Pequeña pausa antes de leer los metadatos
+            
             metadatos_general = leer_zattrs_de_grupo(ZARR_PATH, "")
             tracks = get_track_names_simplified(ZARR_PATH)
+
             tracks_updated = []
             dataframes = {}
+        
+            if DEMO:
+                Frame = FRAME_SIGNAL_DEMO
+            else:
+                Frame = FRAME_SIGNAL
+            
             for track in tracks:
-                metadatos_track = leer_zattrs_de_grupo(ZARR_PATH, FRAME_SIGNAL_DEMO + track)
+                metadatos_track = leer_zattrs_de_grupo(ZARR_PATH, Frame + track)
                 if metadatos_track is not None:
                     if metadatos_track.get("last_updated") == metadatos_general.get("last_updated"):
                         print(f"   - La track '{track}' fue actualizada.")
                         print(f"        - Ultima actualización contiene datos de {metadatos_track.get('last_update_secs')} segundos que se representan en {metadatos_track.get('last_update_samples')} samples")
-                        sample = FRAME_SIGNAL_DEMO + track
+                        sample = Frame + track
                         df_track = leer_ultimas_muestras_zarr(ZARR_PATH, sample, metadatos_track.get('last_update_samples'))
                         dataframes[sample.removeprefix("signals/")] = df_track
                         print(f"        - Se guarda el dataframe en la lista.")
@@ -193,38 +208,57 @@ if __name__ == "__main__":
             
             results = {}
 
-# Falten per importar: Driving Pressure, Dynamic Compliance, ROX Index, Temp Comparison, Systemic Vascular Resistance, 
-            # Cardiac Power Output, Effective Arterial Elastance
-            # O no els trobo jo o no estan al github (potser tenen noms raros i nose identificarlos)
+# Falten per importar: La llibreria dels BPV i HRV (falta la llibreria aquella, ecgdetectors)
 
             for algoritme in list_available:
                 match algoritme:
                     case 'Shock Index':
                         results['Shock Index'] = ShockIndex(dataframes).values
-#                    case 'Driving Pressure':
-#                        results['Driving Pressure'] = DrivingPressure(dataframes).values
-#                    case 'Dynamic Compliance':
-#                        results['Dynamic Compliance'] = DynamicCompliance(dataframes).values
-#                    case 'ROX Index':
-#                        results['ROX Index'] = RoxIndex(dataframes).values
-#                    case 'Temp Comparison':
-#                        results['Temp Comparison'] = TempComparison(dataframes).values
+                    case 'Driving Pressure':
+                        results['Driving Pressure'] = DrivingPressure(dataframes).values
+                    case 'Dynamic Compliance':
+                        results['Dynamic Compliance'] = DynamicCompliance(dataframes).values
+                    case 'ROX Index':
+                        results['ROX Index'] = RoxIndex(dataframes).values
+                    case 'Temp Comparison':
+                        results['Temp Comparison'] = TempComparison(dataframes).values
                     case 'Cardiac Output':
                         results['Cardiac Output'] = CardiacOutput(dataframes).values
-#                    case 'Systemic Vascular Resistance':
-#                        results['Systemic Vascular Resistance'] = SystemicVascularResistance(dataframes).values
-#                    case 'Cardiac Power Output':
-#                        results['Cardiac Power Output'] = CardiacPowerOutput(dataframes).values
-#                    case 'Effective Arterial Elastance':
-#                        results['Effective Arterial Elastance'] = EffectiveArterialElastance(dataframes).values
-                    case 'Blood Pressure Variability':
-                        results['Blood Pressure Variability'] = BloodPressureVariability(dataframes).values
-                    case 'Heart Rate Variability':
-                        results['Heart Rate Variability'] = HeartRateVariability(dataframes).values
+                    case 'Systemic Vascular Resistance':
+                        results['Systemic Vascular Resistance'] = SystemicVascularResistance(dataframes).values
+                    case 'Cardiac Power Output':
+                        results['Cardiac Power Output'] = CardiacPowerOutput(dataframes).values
+                    case 'Effective Arterial Elastance':
+                        results['Effective Arterial Elastance'] = EffectiveArterialElastance(dataframes).values
+                    # case 'Blood Pressure Variability':
+                    #     results['Blood Pressure Variability'] = BloodPressureVariability(dataframes).values
+                    # case 'Heart Rate Variability':
+                    #     results['Heart Rate Variability'] = HeartRateVariability(dataframes).values
                     case _:
                         print(f"Advertencia: Algoritmo '{algoritme}' no encontrado")
                         pass
-            print(f"Resultados de los algoritmos: {results}") # Estos resultados nose si tengo que passarlos al zarr o ya lo hacen los algoritmos (Se tendra que hablar)
+            print(f"Resultados de los algoritmos: {results}")
+            return results
+                    
+    except KeyboardInterrupt:
+        print("\n--- Monitoreo detenido por el usuario. ---")
+
+if __name__ == "__main__":
+    try:
+        while True:
+            results = main_to_loop()
+
+            for Nombre_algoritmo, df_result in results.items():
+                value_columns = [col for col in df_result.columns if col != 'Timestamp']
+                time_ms_array = df_result['Timestamp'].values
+                
+                visible = False # Bool que dicta si se puede enseñar la función
+                if Nombre_algoritmo in ALGORITMOS_VISIBLES:
+                    visible = True
+                
+                for track_name in value_columns:
+                    value_array = df_result[track_name].values
+                    escribir_prediccion(ZARR_PATH, track_name, time_ms_array, value_array, modelo_info={"model": Nombre_algoritmo, "visibilidad": visible})
 
     except KeyboardInterrupt:
         print("\n--- Monitoreo detenido por el usuario. ---")
