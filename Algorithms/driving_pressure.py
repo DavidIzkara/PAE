987a481e4fccbc586dbeb771added09f6acfa521
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-11-26 22:40:52 +0100

    Update driving_pressure.py

--- Algorithms/driving_pressure.py
+++ Algorithms/driving_pressure.py
@@ -37,12 +37,16 @@ class DrivingPressure:
         pplat = list_dataframe['Intellivue/PPLAT_CMH2O'] 
         peep = list_dataframe['Intellivue/PEEP_CMH2O']
 
+        # Deletes the nan values
+        pplat_clean = pplat[pplat["value"].notna()]
+        peep_clean = peep[peep["value"].notna()]
+
         # Creates a new dataframe with timestamp | pplat_value | peep_value where both values come from the same absolute timestamp
-        pre_dp= pplat.merge(peep, on="t_abs_ms")
+        pre_dp= pplat_clean.merge(peep_clean, on="time_ms")
         #print(pre_dp)
 
         #Creates the DP dataframe: Timestamp | DP_value
-        self.values = pd.DataFrame({'Timestamp': pre_dp["t_abs_ms"], 'DP': pre_dp["values_x"] - pre_dp["values_y"]}) 
+        self.values = pd.DataFrame({'Timestamp': pre_dp["time_ms"], 'DP': pre_dp["value_x"] - pre_dp["value_y"]}) 
 #Does the driving pressure calculation by subtracting PEEP from PLAT.
 #Does not require any special handling of missing data as this class is only used when we have the data.
 #Does not need to handle multiple possible track names as these are fixed.
